e5b22173629f92f8d294b93973315cfd5fab942a
sparky8512 <76499194+sparky8512@users.noreply.github.com>
2026-03-21 19:35:52 -0700

    Service support for v1 reflection protocol
    Modify the copies of v1alpha service files to implement v1 of the service instead. The protocol is the same between v1alpha and v1, other than exporting at a different package path, so this was just a matter of using the v1 protocol files instead of the v1alpha ones.
    
    These service files are only for use in the test framework inside this project and will only be used if the proper v1 protocol and service files cannot be found in the grpcio-reflection package.

--- tests/service/grpc_reflection_v1/_async.py
+++ tests/service/grpc_reflection_v1/_async.py
@@ -16,8 +16,8 @@
 from typing import AsyncIterable
 
 import grpc
-from grpc_reflection.v1alpha import reflection_pb2 as _reflection_pb2
-from grpc_reflection.v1alpha._base import BaseReflectionServicer
+from yagrc.grpc_reflection.v1 import reflection_pb2 as _reflection_pb2
+from ._base import BaseReflectionServicer
 
 
 class ReflectionServicer(BaseReflectionServicer):
